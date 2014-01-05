f2be492d11bdee2e6e1846263c236b069b140390
Lars Hesel Christensen <larshesel@gmail.com>
2014-01-05 14:45:02 +0100

    Add lager and dependencies to application startup

--- src/mtree_app.erl
+++ src/mtree_app.erl
@@ -9,7 +9,22 @@
 %% Application callbacks
 %% ===================================================================
 
+ensure_started(App) ->
+    case application:start(App) of
+        ok ->
+            ok;
+        {error, {already_started, App}} ->
+            ok
+    end.
+
+
 start(_StartType, _StartArgs) ->
+    ensure_started(sasl),
+    ensure_started(os_mon),
+    ensure_started(compiler),
+    ensure_started(syntax_tools),
+    ensure_started(goldrush),
+    ensure_started(lager),
     mtree_sup:start_link().
 
 stop(_State) ->
