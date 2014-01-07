661a75c96f1e37d25e8573c1b7f7982141f6f991
Lars Hesel Christensen <larshesel@gmail.com>
2014-01-07 22:59:42 +0100

    Add logic to build frame with 3 byte hdr field.

--- src/mtree_tcp_proxy.erl
+++ src/mtree_tcp_proxy.erl
@@ -18,7 +18,11 @@
 
 -define(SERVER, ?MODULE).
 
--record(state, {sock}).
+-record(state, {sock, incomplete_frame = <<>>}).
+-type state() :: #state{}.
+
+-define(FRAME_HDR_LEN, 3*8).
+%%-define(FRAME_MAX_SIZE, 16#ffffff).
 
 %%%===================================================================
 %%% API
@@ -44,6 +48,19 @@ handle_call(_Request, _From, State) ->
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
+handle_info({tcp_closed, Sock}, State) ->
+    error_logger:info_msg("sock closed: ~p~n", [Sock]),
+    {stop, normal, State};
+
+handle_info({tcp_error, _Socket, _}, State) ->
+    {stop, normal, State};
+
+handle_info({tcp, Sock, Data}, State) ->
+    State1 = add_to_incoming_frame(State, Data),
+    State2 = maybe_handle_frame(State1),
+    set_active(Sock),
+    {noreply, State2};
+
 handle_info(_Info, State) ->
     {noreply, State}.
 
@@ -57,4 +74,37 @@ code_change(_OldVsn, State, _Extra) ->
 %%% Internal functions
 %%%===================================================================
 
+set_active(Sock) ->
+    inet:setopts(Sock, [{active,once}]).
 
+
+-spec maybe_handle_frame(state()) -> state().
+maybe_handle_frame(State) ->
+    case build_frame(State) of
+	{complete, FrameData, State1} ->
+	    State2 = handle_frame(FrameData, State1),
+	    maybe_handle_frame(State2);
+	{incomplete, State1} ->
+	    State1
+    end.
+
+-spec build_frame(state()) -> {complete, binary(), state()}
+				  | {incomplete, state()}.
+build_frame(#state{incomplete_frame =
+		       <<FrameLen:(?FRAME_HDR_LEN), Frame/binary>>} = State) ->
+    case Frame of
+	<<CompleteFrame:FrameLen/binary, NextFrame/binary>> ->
+	    {complete, CompleteFrame, State#state{incomplete_frame = NextFrame}};
+	 _ ->
+	    error_logger:info_msg("Still incomplete: FrameLen: ~p~n", [byte_size(Frame)]),
+	    error_logger:info_msg("Need ~p bytes~n", [FrameLen]), 
+	    {incomplete, State}
+    end;
+build_frame(State) ->
+    {incomplete, State}.
+
+add_to_incoming_frame(#state{incomplete_frame = Inc} = State, Data) ->
+    State#state{ incomplete_frame = <<Inc/binary, Data/binary>> }.
+
+handle_frame(_FrameData, State) ->
+    State.
