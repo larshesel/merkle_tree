34ae4bae96e8d1b490493a0f89f3126840b95a6e
Lars Hesel Christensen <larshesel@gmail.com>
2014-01-08 11:32:14 +0100

    Rename to _packet where relevant. Add build_packet eunit tests.

--- src/mtree_tcp_proxy.erl
+++ src/mtree_tcp_proxy.erl
@@ -1,4 +1,7 @@
 -module(mtree_tcp_proxy).
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
 
 %% This module receives the incoming client requests coming in over
 %% the tcp connection, decodes them, executes them against the
@@ -80,31 +83,62 @@ set_active(Sock) ->
 
 -spec maybe_handle_frame(state()) -> state().
 maybe_handle_frame(State) ->
-    case build_frame(State) of
-	{complete, FrameData, State1} ->
-	    State2 = handle_frame(FrameData, State1),
+    case build_packet(State) of
+	{complete, Packet, State1} ->
+	    State2 = handle_packet(Packet, State1),
 	    maybe_handle_frame(State2);
 	{incomplete, State1} ->
 	    State1
     end.
 
--spec build_frame(state()) -> {complete, binary(), state()}
+-spec build_packet(state()) -> {complete, binary(), state()}
 				  | {incomplete, state()}.
-build_frame(#state{incomplete_frame =
+build_packet(#state{incomplete_frame =
 		       <<FrameLen:(?FRAME_HDR_LEN), Frame/binary>>} = State) ->
     case Frame of
 	<<CompleteFrame:FrameLen/binary, NextFrame/binary>> ->
 	    {complete, CompleteFrame, State#state{incomplete_frame = NextFrame}};
 	 _ ->
 	    error_logger:info_msg("Still incomplete: FrameLen: ~p~n", [byte_size(Frame)]),
-	    error_logger:info_msg("Need ~p bytes~n", [FrameLen]), 
+	    error_logger:info_msg("Need ~p bytes~n", [FrameLen]),
 	    {incomplete, State}
     end;
-build_frame(State) ->
+build_packet(State) ->
     {incomplete, State}.
 
 add_to_incoming_frame(#state{incomplete_frame = Inc} = State, Data) ->
     State#state{ incomplete_frame = <<Inc/binary, Data/binary>> }.
 
-handle_frame(_FrameData, State) ->
+handle_packet(_FrameData, State) ->
     State.
+
+%% testing
+-ifdef(TEST).
+
+fake_frame_data(FrameData) ->
+    #state{incomplete_frame = FrameData}.
+
+exact_frame_test() ->
+    S = fake_frame_data(<<0,0,3,1,2,3>>),
+    Frame = build_packet(S),
+    ?assertMatch({complete, <<1,2,3>>, #state{incomplete_frame = <<>>}}, Frame).
+
+incomplete_frame_test() ->
+    S = fake_frame_data(<<0,0,3,1,2>>),
+    Frame = build_packet(S),
+    ?assertMatch({incomplete, #state{incomplete_frame = <<0,0,3,1,2>>}}, Frame).
+
+more_than_one_frame_test() ->
+    F1 = <<0,0,3,1,2,3>>,
+    F2 = <<0,0,2,3,4>>,
+    S = fake_frame_data(<<F1/binary, F2/binary>>),
+    Frame = build_packet(S),
+
+    ?assertMatch({complete, <<1,2,3>>, #state{incomplete_frame = <<0,0,2,3,4>>}}, Frame),
+    {_, _, S2} = Frame,
+
+    Frame2 = build_packet(S2),
+
+    ?assertMatch({complete, <<3,4>>, #state{incomplete_frame = <<>>}}, Frame2).
+
+-endif.
