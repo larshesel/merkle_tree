9b10574dc58a3aea867b9431e64ddbe989430ba1
Lars Hesel Christensen <larshesel@gmail.com>
2014-01-09 14:34:13 +0100

    Add write Mekle tree to file.

--- src/mtree_file.erl
+++ src/mtree_file.erl
@@ -1,10 +1,26 @@
 -module(mtree_file).
 
--export([build_tree/1]).
+-export([build_tree/1, build_tree/2, write/2]).
 -include("mtree.hrl").
 
 %% A simple but inefficient way to build a merkle tree over a file.
 -spec build_tree(string()) -> mtree().
 build_tree(Filename) ->
+    build_tree(Filename, 4096).
+
+-spec build_tree(string(), integer()) -> mtree().
+build_tree(Filename, ChunkSize) ->
     {ok, BinData} = file:read_file(Filename),
-    mtree_raw:build_tree(BinData, 4096).
+    mtree_raw:build_tree(BinData, ChunkSize).
+
+write(Tree, Name) ->
+    {ok, IoDev} = file:open(Name, [write, binary]),
+    write_to_handle(Tree, IoDev),
+    ok = file:close(IoDev).
+
+write_to_handle({leaf, Data}, IoDev) ->
+    file:write(IoDev, Data);
+write_to_handle({inner, _, L, R}, IoDev) ->
+    write_to_handle(L, IoDev),
+    write_to_handle(R, IoDev).
+
