15673423be85a505b50217cb0d88a30609b10a6f
Lars Hesel Christensen <larshesel@gmail.com>
2014-01-21 13:50:19 +0100

    Add encode/decode test of protobuff messages and helper functions.

---
+++ src/merkle_tree_pb_util.erl
@@ -0,0 +1,54 @@
+%%% @doc
+%%% A collection of protobuff helper functions.
+%%% @end
+-module(merkle_tree_pb_util).
+-include("merkle_tree_pb.hrl").
+
+%% TODO: only export relevant functions
+-compile(export_all).
+
+-define(HANDSHAKE_REQ, 1).
+-define(ACK_RESP, 2).
+-define(FETCH_REQ, 3).
+-define(LEAVES_RESP, 4).
+-define(FETCHDONE_MSG,5).
+-define(GETHASH_REQ, 6).
+-define(HASH_RESP, 7).
+-define(ERROR_MSG, 8).
+
+create_handshake_req(Major, Minor, Opts) when is_list(Opts) ->
+    #handshakereq{major_version = Major, minor_version = Minor, options = Opts}.
+
+create_ack_resp() ->
+    #ackresp{}.
+
+fetch_req(PosBin) when is_bitstring(PosBin) ->
+    #fetchreq{pos = pos(PosBin)}.
+
+pos(PosBin) when is_bitstring(PosBin)->
+    #pos{pos = bitstring_to_binary_pad(PosBin),
+         num_bits = erlang:bit_size(PosBin)}.
+
+bitstring_to_binary_pad(PosBin) ->
+    NumBits = erlang:bit_size(PosBin),
+    MissingBits = 8 - (NumBits rem 8),
+    <<0:MissingBits, PosBin/bitstring>>.
+
+create_leaf(PosBin, Data) ->
+    #leaf{pos = pos(PosBin),
+          data = Data}.
+
+create_leaves_resp(Leaves) ->
+    #leavesresp{leaves = Leaves}.
+
+create_fetch_done_msg() ->
+    #fetchdonemsg{}.
+
+create_get_hash_req(PosBin) ->
+    #gethashreq{pos = pos(PosBin)}.
+
+create_hash_resp(PosBin, Hash) ->
+    #hashresp{pos = pos(PosBin), hash = Hash}.
+
+create_error_msg(Code, Msg) ->
+    #errormsg{code = Code, msg = Msg}.
---
+++ test/merkle_tree_pb_tests.erl
@@ -0,0 +1,76 @@
+-module(merkle_tree_pb_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+%% test encoding and decoding of the different merkle-tree protocol
+%% messages.
+
+handshake_req_test() ->
+    Pair = merkle_tree_proto:pair(<<"key">>, <<"val">>),
+    Data = merkle_tree_pb_util:create_handshake_req(1,0,[Pair]),
+    gen_test(Data,
+             fun merkle_tree_pb:encode_handshakereq/1,
+             fun merkle_tree_pb:decode_handshakereq/1).
+
+ack_resp_test() ->
+    Data = merkle_tree_pb_util:create_ack_resp(),
+    gen_test(Data,
+             fun merkle_tree_pb:encode_ackresp/1,
+             fun merkle_tree_pb:decode_ackresp/1).
+
+fetch_req_test() ->
+    PosBin = <<1:1, 0:1, 1:1, 1:1, 0:1, 1:1>>,
+    Data = merkle_tree_pb_util:fetch_req(PosBin),
+    gen_test(Data,
+             fun merkle_tree_pb:encode_fetchreq/1,
+             fun merkle_tree_pb:decode_fetchreq/1).
+
+leaf_test() ->
+    Data = gen_leaf(),
+    gen_test(Data,
+             fun merkle_tree_pb:encode_leaf/1,
+             fun merkle_tree_pb:decode_leaf/1).
+
+gen_leaf() ->
+    PosBin = <<500:9>>,
+    LeafData = <<"leafdata">>,
+    merkle_tree_pb_util:create_leaf(PosBin, LeafData).
+
+leaves_test() ->
+    Leaves  = [gen_leaf() || _ <- lists:seq(1,100)],
+    Data = merkle_tree_pb_util:create_leaves_resp(Leaves),
+    gen_test(Data,
+             fun merkle_tree_pb:encode_leavesresp/1,
+             fun merkle_tree_pb:decode_leavesresp/1).
+
+fetch_done_msg_test() ->
+    Data = merkle_tree_pb_util:create_fetch_done_msg(),
+    gen_test(Data,
+             fun merkle_tree_pb:encode_fetchdonemsg/1,
+             fun merkle_tree_pb:decode_fetchdonemsg/1).
+
+get_hash_req_test() ->
+    PosBin = <<234:9>>,
+    Data = merkle_tree_pb_util:create_get_hash_req(PosBin),
+    gen_test(Data,
+             fun merkle_tree_pb:encode_gethashreq/1,
+             fun merkle_tree_pb:decode_gethashreq/1).
+
+hash_resp_test() ->
+    PosBin = <<34:13>>,
+    Hash = <<"a binary representing a hashed value">>,
+    Data = merkle_tree_pb_util:create_hash_resp(PosBin, Hash),
+    gen_test(Data,
+             fun merkle_tree_pb:encode_hashresp/1,
+             fun merkle_tree_pb:decode_hashresp/1).
+
+error_msg_test() ->
+    Code = 3,
+    Msg = "Something went wrong",
+    Data = merkle_tree_pb_util:create_error_msg(Code, Msg),
+    gen_test(Data,
+             fun merkle_tree_pb:encode_errormsg/1,
+             fun merkle_tree_pb:decode_errormsg/1).
+
+gen_test(Data, EncFun, DecFun) ->
+    EncData = erlang:iolist_to_binary(EncFun(Data)),
+    ?assertEqual(Data, DecFun(EncData)).
